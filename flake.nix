6efad2bab27d78ea1facdd09d410ad777508c301
Kodie <kodii.tulip@proton.me>
2026-03-18 02:21:18 -0300

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "Packwiz minecraft mopack enviroment";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    systems.url = "github:nix-systems/default";
+  };
+
+  outputs =
+    { nixpkgs, systems, ... }:
+    let
+      forEachSystem =
+        fn:
+        nixpkgs.lib.genAttrs (import systems) (
+          system:
+          fn (
+            import nixpkgs {
+              inherit system;
+            }
+          )
+        );
+    in
+    {
+      devShells = forEachSystem (pkgs: {
+        default = pkgs.mkShell {
+          packages = [ pkgs.packwiz ];
+        };
+      });
+    };
+}
